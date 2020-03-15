c9dc76c4df1b63636d3879639865cb58053f4ba9
Juliano Lima <julianodgtz@gmail.com>
2020-03-15 17:40:16 -0300

    Remove flow stuff

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -1,4 +1,3 @@
-// @flow strict
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
@@ -7,14 +6,8 @@ import Feed from '../components/Feed';
 import Page from '../components/Page';
 import Pagination from '../components/Pagination';
 import { useSiteMetadata } from '../hooks';
-import type { PageContext, AllMarkdownRemark } from '../types';
 
-type Props = {
-  data: AllMarkdownRemark,
-  pageContext: PageContext
-};
-
-const CategoryTemplate = ({ data, pageContext }: Props) => {
+const CategoryTemplate = ({ data, pageContext }) => {
   const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
 
   const {
