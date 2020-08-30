47cfc217b61c3ff21b4244bd7ea56d1c4e9051ea
Juliano Lima <juliano.lima@nubank.com.br>
2020-08-30 14:41:31 -0300

    Update the Author test

--- src/components/Sidebar/Author/Author.jsx
+++ src/components/Sidebar/Author/Author.jsx
@@ -4,9 +4,10 @@ import { withPrefix, Link } from 'gatsby';
 import styles from './Author.module.scss';
 
 const Author = ({ author, isIndex }) => (
-  <div className={styles.author}>
-    <Link to="/">
+  <div data-testid="author" className={styles.author}>
+    <Link data-testid="author-link" to="/">
       <img
+        data-testid="author-img"
         src={withPrefix(author.photo)}
         className={styles.author__photo}
         width="75"
@@ -16,19 +17,29 @@ const Author = ({ author, isIndex }) => (
     </Link>
 
     {isIndex === true ? (
-      <h1 className={styles.author__title}>
-        <Link className={styles['author__title-link']} to="/">
+      <h1 className={styles.author__title} data-testid="author-title-link-h1">
+        <Link
+          data-testid="author-title-link"
+          className={styles['author__title-link']}
+          to="/"
+        >
           {author.name}
         </Link>
       </h1>
     ) : (
-      <h2 className={styles.author__title}>
-        <Link className={styles['author__title-link']} to="/">
+      <h2 className={styles.author__title} data-testid="author-title-link-h2">
+        <Link
+          data-testid="author-title-link"
+          className={styles['author__title-link']}
+          to="/"
+        >
           {author.name}
         </Link>
       </h2>
     )}
-    <p className={styles.author__subtitle}>{author.bio}</p>
+    <p data-testid="author-bio" className={styles.author__subtitle}>
+      {author.bio}
+    </p>
   </div>
 );
 
--- src/components/Sidebar/Author/Author.test.jsx
+++ src/components/Sidebar/Author/Author.test.jsx
@@ -1,19 +1,62 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, cleanup } from '@testing-library/react';
 import Author from './Author';
 
 describe('Author', () => {
   const props = {
     author: {
-      name: 'test',
+      name: 'Juju',
       photo: '/photo.jpg',
       bio: 'test',
     },
     isIndex: false,
   };
 
-  it('renders correctly', () => {
-    const tree = renderer.create(<Author {...props} />).toJSON();
-    expect(tree).toMatchSnapshot();
+  beforeEach(() => {
+    cleanup();
+  });
+
+  describe('Should show the author image', () => {
+    it('Should have a link to the photo', () => {
+      const { getByTestId } = render(<Author {...props} />);
+      expect(getByTestId('author-img')).toHaveProperty(
+        'src',
+        'http://localhost/photo.jpg'
+      );
+    });
+
+    it('Should have an alt describing the image', () => {
+      const { getByTestId } = render(<Author {...props} />);
+      expect(getByTestId('author-img')).toHaveProperty(
+        'alt',
+        props.author.name
+      );
+    });
+  });
+
+  describe('Should show the author name', () => {
+    it('Should show a larger text when its index', () => {
+      const propsWithIsIndexTrue = { ...props, isIndex: true };
+      const { getByTestId } = render(<Author {...propsWithIsIndexTrue} />);
+      getByTestId('author-title-link-h1');
+      expect(getByTestId('author-title-link').textContent).toBe(
+        props.author.name
+      );
+    });
+
+    it('Should shown a medium message when its not index', () => {
+      const { getByTestId } = render(<Author {...props} />);
+      getByTestId('author-title-link-h2');
+      expect(getByTestId('author-title-link').textContent).toBe(
+        props.author.name
+      );
+    });
+  });
+
+  describe('Should show the last Paragraph', () => {
+    it('Should show the author bio', () => {
+      const { getByTestId } = render(<Author {...props} />);
+      expect(getByTestId('author-bio').textContent).toBe(props.author.bio);
+    });
   });
 });
