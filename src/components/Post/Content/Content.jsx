4aa1eb7b81cfac6009a440fc6f608ec063860354
Juliano Lima <juliano.lima@nubank.com.br>
2020-09-06 00:11:02 -0300

    Adds missing props on Content component

--- src/components/Post/Content/Content.jsx
+++ src/components/Post/Content/Content.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import styles from './Content.module.scss';
 
 const Content = ({ body, title }) => (
-  <div className={styles.content}>
-    <h1 className={styles.content__title}>{title}</h1>
+  <div className={styles.content} data-testid="post-content">
+    <h1 className={styles.content__title} data-testid="post-content-title">{title}</h1>
     <div
       className={styles.content__body}
+      data-testid="post-content-body"
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: body }}
     />
