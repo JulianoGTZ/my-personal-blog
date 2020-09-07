c60c970702224e6ed76a5ac2c49dbc47adb8a4f6
Juliano Lima <juliano.lima@nubank.com.br>
2020-09-06 21:19:42 -0300

    Fix the url on config

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,7 +13,7 @@ const fs = require("fs");
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-rf", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["checkout", "--force", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
   } catch (e) {
