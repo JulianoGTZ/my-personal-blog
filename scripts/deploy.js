ab034b73ae53a429205316e72f4f7812028ab849
Juliano Lima <juliano.lima@nubank.com.br>
2020-09-07 11:25:29 -0300

    Improve scripts to use multi-line scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,6 +2,8 @@ const execa = require("execa");
 
 (async () => {
   try {
+    await execa("git", ["fetch"]);
+    console.log("fetching master")
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     console.log("Building...");
     await execa("yarn", ["build"]);
@@ -12,7 +14,7 @@ const execa = require("execa");
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-rf", folderName]);
-    await execa("git", ["checkout", "--force", "master"]);
+    await execa("git", ["checkout", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed");
   } catch (e) {
