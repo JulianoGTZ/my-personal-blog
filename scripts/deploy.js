31934558e76df7efb770f683f1d76aeb6e2b44b6
Juliano Lima <juliano.lima@nubank.com.br>
2020-09-06 21:16:04 -0300

    Adds deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const execa = require("execa");
+const fs = require("fs");
+
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    console.log("Building...");
+    await execa("yarn", ["build"]);
+    // Understand if it's dist or build folder
+    const folderName = "build";
+    await execa("git", ["add", "--all"]);
+    await execa("git", ["commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-rf", folderName]);
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed");
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
